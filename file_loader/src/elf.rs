0af77f9877d9c0acfefe09a30c34d01d9bb77196
Ray Redondo <rdrpenguin04@gmail.com>
2021-05-15 21:29:32 -0500

    Appease clippy

--- file_loader/src/elf.rs
+++ file_loader/src/elf.rs
@@ -8,16 +8,11 @@ pub struct ElfMemoryMap {}
 impl FileLoader for ElfFileLoader {
     fn can_load(&self, file: &mut dyn ReadSeek) -> bool {
         let mut buf = vec![0u8; 6];
-        match file.read_exact(&mut buf) {
-            Err(_) => return false,
-            _ => {}
-        };
-        file.seek(SeekFrom::Start(0)).unwrap();
-        if let &[0x7F, b'E', b'L', b'F', _, 1] = &*buf {
-            true
-        } else {
-            false
+        if file.read_exact(&mut buf).is_err() {
+            return false;
         }
+        file.seek(SeekFrom::Start(0)).unwrap();
+        matches!(&*buf, &[0x7F, b'E', b'L', b'F', _, 1])
     }
 
     fn load(&self, _file: &mut dyn Read) -> Box<dyn MemoryMap> {
