77812ab92958af595d2ba486ac33b934aa41fc9f
Ray Redondo <rdrpenguin04@gmail.com>
2021-06-04 08:29:02 -0500

    AAAA

--- processor/src/amd64/interp.rs
+++ processor/src/amd64/interp.rs
@@ -1,3 +1,4 @@
+use bitflags::bitflags;
 use crate::ProcessorImplementation;
 use file_loader::MemoryMap;
 
@@ -6,6 +7,13 @@ pub struct Amd64Interp {}
 impl Amd64Interp {
     pub fn new() -> Amd64Interp {
         Amd64Interp {}
+    }
+}
+
+bitflags! {
+    struct Prefixes: u64 {
+        const NONE = 0b0000;
+        const REP = 0b0001;
     }
 }
 
@@ -18,8 +26,18 @@ impl ProcessorImplementation for Amd64Interp {
     }
     fn tick(&mut self, map: &mut dyn MemoryMap) {
         let regs = map.registers().regs64.as_ref().unwrap();
-        match map.read_u8(regs.rip) {
-            _ => panic!("Unrecognized instruction!"),
+        let mut prefixes = Prefixes::NONE;
+        let done = false;
+        while !done {
+            let increment_ip = true;
+            let instr = map.read_u8(regs.rip);
+            match instr {
+                0xF3 => prefixes |= Prefixes::REP,
+                _ => panic!("Unrecognized instruction {:#04X}", instr),
+            }
+            if increment_ip {
+                map.registers_mut().regs64.unwrap().rip += 1;
+            }
         }
     }
 }
