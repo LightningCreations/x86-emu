a78ab1332d5c45927d16c25eebb3d2430e99ba11
Ray Redondo <rdrpenguin04@gmail.com>
2021-06-04 09:17:32 -0500

    That's a thing

--- processor/src/amd64/interp.rs
+++ processor/src/amd64/interp.rs
@@ -26,12 +26,25 @@ impl ProcessorImplementation for Amd64Interp {
     }
     fn tick(&mut self, map: &mut dyn MemoryMap) {
         let mut prefixes = Prefixes::NONE;
-        let done = false;
+        let mut done = false;
         while !done {
             let regs = map.registers();
             let increment_ip = true;
             let instr = map.read_u8(regs.rip);
             match instr {
+                0x0F => {
+                    map.registers_mut().rip += 1;
+                    let regs = map.registers();
+                    let instr2 = map.read_u8(regs.rip);
+                    match instr2 {
+                        0x1E => {
+                            let _ = map.read_u8(regs.rip+1); // ModR/M
+                            map.registers_mut().rip += 1;
+                            done = true // It's either a NOP or an ENDBR64, neither of which we care about
+                        }
+                        _ => panic!("Unrecognized instruction 0x0F{:02X}", instr2),
+                    }
+                }
                 0xF3 => prefixes |= Prefixes::REP,
                 _ => panic!("Unrecognized instruction {:#04X}", instr),
             }
